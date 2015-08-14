8b374e233581f81a9cd3c8993591ef1f34eb67b0
mcarver <mcarver@gmail.com>
2015-08-14 08:08:45 -0400

    fix whitespacing issue.

--- background.js
+++ background.js
@@ -1,13 +1,13 @@
 var vhost = "",
     enabled = false,
-		requestFilter = {urls: ["<all_urls>"]},
-		extraInfoSpec = ["blocking", "requestHeaders"],
-		eventHandler = function(details) {
-			if (enabled)
-			{
-				details.requestHeaders.push({ name: "Host", value: vhost });
-				return {requestHeaders: details.requestHeaders};
-			}
-		};
+    requestFilter = {urls: ["<all_urls>"]},
+    extraInfoSpec = ["blocking", "requestHeaders"],
+    eventHandler = function(details) {
+      if (enabled)
+      {
+        details.requestHeaders.push({ name: "Host", value: vhost });
+        return {requestHeaders: details.requestHeaders};
+      }
+    };
 
 chrome.webRequest.onBeforeSendHeaders.addListener(eventHandler, requestFilter, extraInfoSpec);
